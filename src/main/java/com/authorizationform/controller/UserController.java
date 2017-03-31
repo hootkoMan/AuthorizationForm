106d1652e23876a75c94a0f7f711cd655f18ea24
Ruslan <ruslan.bur@gmail.com>
2017-03-31 18:58:20 +0300

    create user controller

---
+++ src/main/java/com/authorizationform/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.authorizationform.controller;
+
+import com.authorizationform.model.User;
+import com.authorizationform.service.SecurityService;
+import com.authorizationform.service.UserService;
+import com.authorizationform.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by ruslan on 31.03.17.
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm,
+                               BindingResult bindingResult,
+                               Model model) {
+        userValidator.validate(userForm, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        userService.save(userForm);
+        securityService.autoLogin(userForm.getUserName(), userForm.getConfirmPassword());
+        return "redirect:/welcome";
+    }
+}
+
+
