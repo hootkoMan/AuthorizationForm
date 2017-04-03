e6551a3b992a6f63570e706ed4f827ea75413786
Ruslan <ruslan.bur@gmail.com>
2017-04-03 16:30:34 +0300

    added to user controller: login, /, welcome, admin

--- src/main/java/com/authorizationform/controller/UserController.java
+++ src/main/java/com/authorizationform/controller/UserController.java
@@ -45,6 +45,29 @@ public class UserController {
         securityService.autoLogin(userForm.getUserName(), userForm.getConfirmPassword());
         return "redirect:/welcome";
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null) {
+            model.addAttribute("error", "Username or password is incorrect");
+        }
+
+        if (logout != null) {
+            model.addAttribute("message", "Logged out successfully");
+        }
+
+        return "login";
+    }
+
+    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    public String welcome(Model model) {
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String admin(Model model) {
+        return "admin";
+    }
 }
 
 
